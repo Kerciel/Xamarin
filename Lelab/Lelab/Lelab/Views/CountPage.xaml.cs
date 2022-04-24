fb0d6f700829a1d8931ac2f45d3fa6174506b11d
kerciel <abdou.fazul.97610@gmail.com>
2022-04-24 20:32:19 +0200

    Ajoutez des fichiers projet.

---
+++ Lelab/Lelab/Lelab/Views/CountPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lelab.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CountPage : ContentPage
+    {
+        int count = 0;
+        public CountPage()
+        {
+            InitializeComponent();
+        }
+
+         void CountButtonClick(object sender, EventArgs e)
+        {
+            count++;
+            Countlelab.Text = count.ToString();//on recupere la valeur de Countlelab qu on a defini dans le X:name
+            Console.WriteLine("BUTTON MARCHE");
+        }
+    }
+}
